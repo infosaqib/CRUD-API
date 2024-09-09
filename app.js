91fbfdc27f1ca0608506b246862069128423edd2
Saqib <hafizsaqi555@gmail.com>
2024-09-09 19:30:14 +0500

    config routinh

--- app.js
+++ app.js
@@ -1,95 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
-const Product = require('./models/product.model')
-const productRoute = require('./routes/product.route')
+const mongoose = require("mongoose");
+const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
 
-// Database Connection
-const connectDB = require('./db');
+//? Database Connection
+const connectDB = require("./db");
 connectDB();
 
-//Middleswares
-app.use(express.json()) 
-app.use(express.urlencoded({extended: false}))
-app.use('/api/products', productRoute)
+//? Middleswares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//? R O U T E S
+//? Routes
+app.use("/api/products", productRoute);
 
-app.get('/', (req, res) => {
-    res.send('APP is running on the Server')
+app.get("/", (req, res) => {
+  res.send("APP is running on the Server");
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-//* Update API
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if (!product) {
-            res.status(404).json({ message: "Product not found" })
-        }
-
-        const updatedProduct = await Product.findById(id)
-        res.status(200).json(updatedProduct)
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-
-    }
-})
-
-
-//*Delete API
-
-app.delete('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            res.status(404).json({ message: "Product not found" })
-        }
-
-        res.status(200).json({ message: "Product deleted successfully!" })
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-
-    }
-})
-
-
-
-
-//Starting Server
+//? Starting Server
 app.listen(port, () => {
-    console.log(`App is running on http://localhost:${port}`);
+  console.log(`App is running on http://localhost:${port}`);
 });
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,76 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const Product = require('../models/product.model')
+const { getProducts, getSingleProduct, createProduct, updateProduct, deleteProduct } = require('../controllers/product.controller');
 
 
 //READ ALL
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+router.get('/', getProducts)
 
 //READ BY ID
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+router.get('/:id', getSingleProduct)
 
 //CREATE API
-router.post('/', async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+router.post('/', createProduct)
 
 //UPDATE API
-router.put('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if (!product) {
-            res.status(404).json({ message: "Product not found" })
-        }
-
-        const updatedProduct = await Product.findById(id)
-        res.status(200).json(updatedProduct)
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-
-    }
-})
+router.put('/:id', updateProduct)
 
 
 //DELETE API
-router.delete('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            res.status(404).json({ message: "Product not found" })
-        }
-
-        res.status(200).json({ message: "Product deleted successfully!" })
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-
-    }
-});
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
